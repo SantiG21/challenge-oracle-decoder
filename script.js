5bd0e66f7e42731d0d546f91ac36eeef1800afee
Santiago Serrano <santiagosg0706@gmail.com>
2022-02-01 22:08:42 -0500

    listo

---
+++ script.js
@@ -0,0 +1,70 @@
+/*reglas de Encriptacion:
+"e" es convertido para "enter"
+"o" es convertido para "ober"
+"i" es convertido para "imes"
+"a" es convertido para "ai"
+"u" es convertido para "ufat"
+Solo letras minusculas
+No se permite acentuacion de palabras
+*/
+
+/*reglas de Desencriptacion:
+"enter" es convertido para "e"
+"ober" es convertido para "o"
+"imes" es convertido para "i"
+"ai" es convertido para "a"
+"ufat" es convertido para "u"
+Solo letras minusculas
+No se permite acentuacion de palabras
+*/
+
+function encriptar() {
+
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+    var txtCifrado = texto.replace(/e/igm,"enter");
+    var txtCifrado = txtCifrado.replace(/o/igm,"ober");
+    var txtCifrado = txtCifrado.replace(/i/igm,"imes");
+    var txtCifrado = txtCifrado.replace(/a/igm,"ai");
+    var txtCifrado = txtCifrado.replace(/u/igm,"ufat");
+
+    document.getElementById("imgDer").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+}
+
+function desencriptar() {
+
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+    var txtCifrado = texto.replace(/enter/igm,"e");
+    var txtCifrado = txtCifrado.replace(/ober/igm,"o");
+    var txtCifrado = txtCifrado.replace(/imes/igm,"i");
+    var txtCifrado = txtCifrado.replace(/ai/igm,"a");
+    var txtCifrado = txtCifrado.replace(/ufat/igm,"u");
+
+    document.getElementById("imgDer").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+}
+
+function copy() {
+    var contenido = document.querySelector("#texto2");
+    contenido.select();
+    document.execCommand("copy");
+    alert("¡Se copió!");
+}
+
+
+
+    
+
+
+
+
+
+
+
+
